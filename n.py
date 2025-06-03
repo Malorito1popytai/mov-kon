c69aada79b9f9f999c94771c5cebb169731d81a7
Malorito1popytai <vasilevtotemchek@gmail.com>
2025-06-03 15:19:46 +0300

    update

--- n.py
+++ n.py
@@ -1,12 +1,33 @@
-from rich.console import Console
-from rich.table import Table
 
-console = Console()
+import os
+import time
 
-table = Table(title="Example Table")
-table.add_column("Column 1", style="bold")
-table.add_column("Column 2")
-table.add_row("Value 1", "Value 2")
-table.add_row("Value 3", "Value 4")
+def clean_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-console.print(table)
+options = ["Сканировать URL", "Проверка с использованием VirusTotal", "Выход"]
+
+while True:
+        clean_screen()    
+        for i, option in enumerate(options, 1):
+            print(f"{i}. {option}")
+
+        choice = input(f"Выберите действие (1-{len(options)}): ").strip()
+        
+        if choice.isdigit() and 1 <= int(choice) <= len(options):
+            choice = int(choice)
+            selected = options[choice - 1]
+            clean_screen()
+            
+            if selected == "Сканировать URL":
+                print("asdasd")
+            elif selected == "Проверка с использованием VirusTotal":
+                print("asdasd")
+            elif selected == "Выход":
+                print("Выход из программы")
+                time.sleep(0.5)
+                break
+        else:
+            clean_screen()
+            print("Неверный выбор! Попробуйте снова.")
+            input("\nНажмите Enter для продолжения...")
