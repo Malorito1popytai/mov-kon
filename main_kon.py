bfa9bd529497beb072aee80a7475f211b45cfe3c
Malorito1popytai <vasilevtotemchek@gmail.com>
2025-06-03 18:36:10 +0300

    big gui update

--- main_kon.py
+++ main_kon.py
@@ -1,8 +1,11 @@
-from colorama import init, Fore
-from colorama import Back
-from colorama import Style
 from datetime import datetime, time
 from dotenv import load_dotenv
+from rich.markdown import Markdown
+from rich.console import Console
+from rich.box import ROUNDED
+from rich.text import Text
+from rich.panel import Panel
+from rich.table import Table
 import requests
 import os
 import random
@@ -10,7 +13,7 @@ import getpass
 import time
 
 load_dotenv()
-init()
+console = Console()
 
 api_key = os.getenv('API_KEY')
 url_weather = f'http://api.openweathermap.org/data/2.5/weather?lat=44.9572&lon=34.1108&appid={api_key}&units=metric'
@@ -35,12 +38,43 @@ wordrand = day_today()
 
 def hello_time(hours):
     if hours > 17:
-        print(f"\n{Style.BRIGHT}{Fore.WHITE}Good Evening, {Fore.YELLOW}favor{Style.RESET_ALL}.")
+        panel_content = Text()
+        panel_content.append(f"Good Evening, ", style="bold white")
+        panel_content.append(f"{user}", style="bold yellow")
+        panel_content.append(f"\n    It's",style="bold white")
+        panel_content.append(f" {time_str}", style="bold cyan")
+        panel_content.append(f" on a ", style="bold white")
+        panel_content.append(f"{wordrand}", style="bold magenta")
+        panel_content.append(f" {datetime.now().strftime('%A')}!", style="bold blue")
     elif hours > 12:
-        print(f"\n{Style.BRIGHT}{Fore.WHITE}Good Afternoon, {Fore.YELLOW}favor{Style.RESET_ALL}.")
+        panel_content = Text()
+        panel_content.append(f"Good Afternoon, ", style="bold white")
+        panel_content.append(f"{user}", style="bold yellow")
+        panel_content.append(f"\n    It's",style="bold white")
+        panel_content.append(f" {time_str}", style="bold cyan")
+        panel_content.append(f" on a ", style="bold white")
+        panel_content.append(f"{wordrand}", style="bold magenta")
+        panel_content.append(f" {datetime.now().strftime('%A')}!", style="bold blue")
     else:
-        print(f"\n{Style.BRIGHT}{Fore.WHITE}Good Moring, {Fore.YELLOW}favor{Style.RESET_ALL}.")
-    print(f"{Style.BRIGHT}{Fore.WHITE}    It's {Fore.CYAN}{time_str}{Fore.WHITE} on a {Fore.MAGENTA}{wordrand}{Fore.BLUE} {datetime.now().strftime("%A")}!{Style.RESET_ALL}\n")
+        panel_content = Text()
+        panel_content.append(f"Good Moring, ", style="bold white")
+        panel_content.append(f"{user}", style="bold yellow")
+        panel_content.append(f"\n    It's",style="bold white")
+        panel_content.append(f" {time_str}", style="bold cyan")
+        panel_content.append(f" on a ", style="bold white")
+        panel_content.append(f"{wordrand}", style="bold magenta")
+        panel_content.append(f" {datetime.now().strftime('%A')}!", style="bold blue")
+    
+    panel = Panel(
+            panel_content,
+            title="[bold]WELCOME TO SYSTEM, FAVOR![/bold]",
+            border_style="green",
+            title_align="center",
+            box=ROUNDED,
+            padding=(1, 2),
+            expand=False
+            )
+    console.print(panel)
 
 def hello_weather():
     try:
@@ -48,34 +82,33 @@ def hello_weather():
         if response.status_code == 200:
             return response.json()
         else:
-            print(f"{Fore.RED}Ошибка API: {response.status_code}, {response.text}{Style.RESET_ALL}")
+            console.print(f"[bold red]Ошибка API: {response.status_code}, {response.text}[/bold red]")
             return None
     except Exception as e:
-        print(f"Ошибка: {e}")
+        console.print(f"[bold]Ошибка: {e}[/blod]")
         return None
 
 
 def humidity1():
     if data['main']['humidity'] > 70:
-        print(f"{Style.BRIGHT}{Fore.BLUE}Влажность: {data['main']['humidity']}%{Style.RESET_ALL}")
+        console.print(f"[bold #0022ff]Влажность: {data['main']['humidity']}%[/]")
     elif data['main']['humidity'] < 45:
-        print(f"{Style.DIM}{Fore.CYAN}Влажность: {data['main']['humidity']}%{Style.RESET_ALL}")
+        console.print(f"[bold #0080ff]Влажность: {data['main']['humidity']}%[/]")
     else:
-        print(f"{Style.BRIGHT}{Fore.CYAN}Влажность: {data['main']['humidity']}%{Style.RESET_ALL}")
+        console.print(f"[bold #005eff]Влажность: {data['main']['humidity']}%[/]")
 
 
 def temperature():
     if data['main']['temp'] > 26:
-        print(f"{Style.BRIGHT}{Fore.RED}Температура (C): {data['main']['temp']}°C{Style.RESET_ALL}")
+        console.print(f"[bold #ff4d00]Температура (C): {data['main']['temp']}°C[/]")
     elif data['main']['temp'] < 13:
-        print(f"{Style.BRIGHT}{Fore.CYAN}Температура (C): {data['main']['temp']}°C{Style.RESET_ALL}")
+        console.print(f"[bold #00ff84]Температура (C): {data['main']['temp']}°C[/]")
     else:
-        print(f"{Style.BRIGHT}{Fore.YELLOW}Температура (C): {data['main']['temp']}°C{Style.RESET_ALL}")
-
+        console.print(f"[bold #ffd500]Температура (C): {data['main']['temp']}°C[/]")
 data = hello_weather()
 
 def main_weather():
-    print(f"{Style.BRIGHT}{Fore.CYAN}Детальный статус: {Fore.MAGENTA}{data['weather'][0]['description']}{Style.RESET_ALL}")
+    console.print(f"\n[bold cyan]Детальный статус: [/bold cyan][bold magenta]{data['weather'][0]['description']}[/bold magenta]")
     humidity1()
     temperature()
 
@@ -87,24 +120,21 @@ def log_main():
         if log_pas == pas:
             clean_screen()
             time.sleep(0.5)
-            print(f"\n{Style.BRIGHT}{Fore.WHITE}WELCOME TO SYSTEM, FAVOR!{Style.RESET_ALL}")
-            print("=========================")
             time.sleep(0.5)
             hello_time(hours)
-            print("=========================")
         else:
             clean_screen()
-            print(f"\n{Style.BRIGHT}{Fore.RED}НЕВЕРНЫЙ ЛОГИН ИЛИ ПАРОЛЬ!{Style.RESET_ALL}")
+            console.print(f"\n[bold red]НЕВЕРНЫЙ ЛОГИН ИЛИ ПАРОЛЬ![/bold red]")
             time.sleep(1)
-            print("\nНажмите Enter, чтобы закрыть программу...")
+            console.print("\n[bold white]Нажмите Enter, чтобы закрыть программу...[/bold white]")
             input()
             exit()
             
     else:
         clean_screen()
-        print(f"\n{Style.BRIGHT}{Fore.RED}НЕВЕРНЫЙ ЛОГИН ИЛИ ПАРОЛЬ!{Style.RESET_ALL}")
+        console.print(f"\n[bold red]НЕВЕРНЫЙ ЛОГИН ИЛИ ПАРОЛЬ![/bold red]")
         time.sleep(1)
-        print("\nНажмите Enter, чтобы закрыть программу...")
+        console.print("\n[bold white]Нажмите Enter, чтобы закрыть программу...[/bold white]")
         input()
         exit()
 
@@ -113,7 +143,7 @@ def log_main():
 if __name__ == '__main__':
     log_main()
     time.sleep(0.5)
-    #main_weather()
+    main_weather()
 
 #tqdm    
 #Будьте терпимы к другим и строги к себе, осознавайте, что вы можете контролировать, а что нет, и игнорируйте людей, которые находятся во власти собственных негативных эмоций.
--- test.py
+++ test.py
@@ -1,10 +1,73 @@
+'''
 from gnews import GNews
+import os
+from rich.console import Console
+from rich.panel import Panel
 
-google_news = GNews(language='us', country='US', max_results=10)
-news = google_news.get_news('zvo')
-for article in news:
-    print(f"Заголовок: {article['title']}")
-    print(f"Источник: {article['publisher']['title']}")
-    print(f"Ссылка: {article['url']}")
-    print("-" * 40)
+console = Console()
 
+def clean_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def gnew():
+   google_news = GNews(language='us', country='US', max_results=10)
+   news = google_news.get_news('zvo')
+   for article in news:
+        print(f"Заголовок: {article['title']}")
+        print(f"Источник: {article['publisher']['title']}")
+        print(f"Ссылка: {article['url']}")
+        print("-" * 40)
+
+
+import os
+import time
+from rich.markdown import Markdown
+from rich.console import Console
+from rich.text import Text
+from rich.panel import Panel
+from rich.table import Table
+
+console = Console()
+wordrand = "1111"
+time_str = "22222"
+
+console.print("Стандартный красный", style="red")
+console.print("Ярко-зелёный", style="bright_green")
+console.print("Жирный циановый на синем", style="bold cyan on blue")
+console.print("Кастомный RGB", style="#ff4500 on #000080")
+console.print("256-цвет", style="color(200) on color(50)")
+console.print("Жирный и курсив", style="bold italic yellow")
+console.print("[bold red]Ошибка![/bold red] [italic green]Успех![/italic green]")
+console.print("Центрированный текст :rocket:", style="bold magenta", justify="center")
+console.print("Длинный текст..." * 10, style="green", overflow="ellipsis")
+
+
+console.print("Жирный красный на чёрном", style="bold red on black")
+console.print("[underline cyan on blue]Подчёркнутый циановый на синем[/underline cyan on blue]")
+
+
+table = Table(border_style="blue")
+table.add_column("Заголовок", style="cyan")
+table.add_row("Данные", style="yellow")
+console.print(table)
+'''
+
+import os
+import time
+from rich.console import Console
+from rich.panel import Panel
+from rich.text import Text
+from rich.box import ROUNDED
+
+console = Console()
+panel = Panel(
+    "Сканировать URL\n[bold white]    It's [/bold white][bold cyan][/bold cyan][bold white] on a [/bold white][bold magenta][/bold magenta][bold blue]![/bold blue]\n,\nВыход",
+    title="Меню",
+    border_style="green",
+    title_align="center",
+    box=ROUNDED,
+    padding=(1, 2),
+    expand=False
+)
+console.print(panel)
